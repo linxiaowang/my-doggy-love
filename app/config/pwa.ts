2e56b38a5bcd47479010d5a9f9d42817215a40c3
linxiaowang <1104220717@qq.com>
2025-11-02 08:42:08 +0800

    Add PWA configuration and icons for improved web app experience
    - Update `pwa.ts` to include additional PWA properties: `background_color`, `display`, `start_url`, and `orientation`.
    - Add new maskable icon and 192x192 icon images to the public directory for PWA support.

--- app/config/pwa.ts
+++ app/config/pwa.ts
@@ -15,6 +15,10 @@ export const pwa: ModuleOptions = {
     short_name: appName,
     description: appDescription,
     theme_color: '#ffffff',
+    background_color: '#ffffff',
+    display: 'standalone',
+    start_url: '/',
+    orientation: 'portrait',
     icons: [
       {
         src: 'pwa-192x192.png',
